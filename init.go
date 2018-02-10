29542f57841ba7445b8c190de366deddf2b2b210
Unknown <farukarakaya@gmail.com>
2018-02-10 22:13:45 +0300

    Page Rank data types initialized

--- init.go
+++ init.go
@@ -1,13 +1,39 @@
 package main
-import "fmt"
-import "./MathFunc"
+import (
+"fmt"
+"./MathFunc"
+)
+
+const beta float32 = 0.85
+var rnew []float32
+type datarow struct {
+	destination []int
+}
+
+var data []datarow
+func addrow(outgoingNodes []int) {
+	s1 := datarow{destination : outgoingNodes}
+	data = append(data,s1)
+}
+func initData() {
+	rnew = make([]int ,len(data)) 
+	MathFunc.ArrInit((1-beta)/len(data),rnew)
+}
+func calcPageRank() {
+	rold
+	for i := 0; i < len(data); i++ {
+		
+	}
+}
+
 
 func main() {
-  var testArr []float32
-  testArr = make([]float32,2)
-  testArr[0] = 1
-  testArr[1] = 100
+  	var testArr []int
+  	testArr = make([]int,2)
+  	testArr[0] = 1
+  	testArr[1] = 100
 
-  testArr = MathFunc.ArrMult(testArr, 2);
-	fmt.Println(testArr)
+  	addrow(testArr)
+	fmt.Println("Hello, Bitches")
+	fmt.Println(data)
 }
