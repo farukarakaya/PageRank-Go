191347e89cfd65b614f0dcc747445d3bd0eb9e26
Serhan Gürsoy <gursoyserhan@yandex.com>
2018-02-12 21:05:56 +0300

    Server-side Request Handling problem solved. Added extra security checks

--- Server/Server.go
+++ Server/Server.go
@@ -1,32 +1,53 @@
 package Server
 
-import(
-	"fmt"
-	"net/http"
+import (
 	"../PageRank"
 	"../Wrapper"
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
-	"log"
+	//"io/ioutil"
+	"net/http"
 )
 
+type REQ_HANDLER struct {
+	URL string
+}
+
 func serveRest(w http.ResponseWriter, r *http.Request) {
-	param :=(mux.Vars(r))
-	fmt.Println(param["link"])
-		destinations, urls := Wrapper.Get2DArray(param["link"])
-	//fmt.Println(r.URL.Path[1:len(r.URL.Path)])
-	str := PageRank.GetPageRankJson(urls,destinations)
-	fmt.Fprintf(w,str)
-	fmt.Println(str)
+	var reqFromUser REQ_HANDLER
+	err := json.NewDecoder(r.Body).Decode(&reqFromUser)
+
+	if RequestChecks(reqFromUser, err) {
+		fmt.Println(err, " URL: ", reqFromUser.URL)
+		str := ProblemHandler(reqFromUser.URL)
+		fmt.Fprintf(w, str)
+	} else {
+		w.WriteHeader(404)
+		fmt.Println(err, " User Req URL:", reqFromUser)
+		w.Write([]byte(`{"message": "Need required parameters. Check API documentation"}`))
+	}
 }
-	 
-func ServeJson(){
+
+func RequestChecks(req REQ_HANDLER, err error) bool {
+	if err != nil {
+		return false
+	}
+	if len(req.URL) == 0 {
+		return false
+	}
+
+	return true
+}
+
+func ProblemHandler(url string) string {
+	destinations, urls := Wrapper.Get2DArray(url)
+	str := PageRank.GetPageRankJson(urls, destinations)
+	return str
+}
+
+func ServeJson() {
 	router := mux.NewRouter()
-	router.HandleFunc("/{link}", serveRest).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/api/getPageRank", serveRest).Methods("POST")
+	http.ListenAndServe(":8080", router)
 }
-
-
-
-
-
