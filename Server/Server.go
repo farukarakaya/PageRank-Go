4faa3ce324aa3d2b00d18db1c60f1ad0a606e300
Unknown <farukarakaya@gmail.com>
2018-02-12 19:17:49 +0300

    Server updated

--- Server/Server.go
+++ Server/Server.go
@@ -3,30 +3,29 @@ package Server
 import(
 	"fmt"
 	"net/http"
+	"../PageRank"
+	"../Wrapper"
+	"github.com/gorilla/mux"
+	"log"
 )
 
-//User defines model for storing account details in database
-var jsonToServe string
-var urlToParse string
-type Link struct {
-	Link string
-}
 func serveRest(w http.ResponseWriter, r *http.Request) {
-	response := jsonToServe
-	urlToParse = r.URL.Path[1:len(r.URL.Path)])
-	fmt.Fprintf(w,response)
+	param :=(mux.Vars(r))
+	fmt.Println(param["link"])
+		destinations, urls := Wrapper.Get2DArray(param["link"])
+	//fmt.Println(r.URL.Path[1:len(r.URL.Path)])
+	str := PageRank.GetPageRankJson(urls,destinations)
+	fmt.Fprintf(w,str)
+	fmt.Println(str)
 }
 	 
-func ServeJson(jts string){
-	jsonToServe = jts
+func ServeJson(){
+	router := mux.NewRouter()
+	router.HandleFunc("/{link}", serveRest).Methods("POST")
 
-	http.HandleFunc("/", serveRest)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-	http.ListenAndServe("localhost:8080", nil)
-}
-func GetUrl() {
-	return urlToParse
-}
 
 
 
--- init.go
+++ init.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"./PageRank"
+	//"fmt"
 	"./Server"
 )
 func main() {
-	adrs := []string{"www.1.com","www.2.com","www.3.com"}
-	dests := [][]int{{0,1},{0,2},{1}}
+	//adrs := []string{"www.1.com","www.2.com","www.3.com"}
+	//dests := [][]int{{0,1},{0,2},{1}}
 	
 	//fmt.Println(adrs)
 	//fmt.Println(dests)
-	str := PageRank.GetPageRankJson(adrs,dests)
-	fmt.Println(str)
-	Server.ServeJson(str)
+	Server.ServeJson()
 }
