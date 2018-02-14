948d2b475220d1707eac47ecba3d94353dbc2f01
Serhan Gürsoy <gursoyserhan@yandex.com>
2018-02-14 05:58:31 +0300

    Server Changes

--- Server/Server.go
+++ Server/Server.go
@@ -6,14 +6,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"net/http"
 	"io/ioutil"
+	"net/http"
+	"os"
 	s "strings"
-	//"os"
 )
+
 type REQ_HANDLER struct {
 	URL string
 }
+
 func serveRest(w http.ResponseWriter, r *http.Request) {
 	var reqFromUser REQ_HANDLER
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -28,7 +30,8 @@ func serveRest(w http.ResponseWriter, r *http.Request) {
 	if RequestChecks(reqFromUser, err) {
 		fmt.Println(err, " URL: ", reqFromUser.URL)
 		go ProblemHandler(reqFromUser.URL)
-		return 
+		w.WriteHeader(200)
+		return
 	} else {
 		w.WriteHeader(404)
 		fmt.Println(err, " User Req URL:", reqFromUser)
@@ -49,20 +52,29 @@ func RequestChecks(req REQ_HANDLER, err error) bool {
 
 func ProblemHandler(url string) {
 	destinations, urls := Wrapper.Get2DArray(url)
-	str := PageRank.GetPageRankJson(urls, destinations)
+
 	if s.HasPrefix(url, "https://") {
-			url = s.Split(url, "https://")[1]
-		} else {
-			url = s.Split(url, "http://")[1]
-		}
+		url = s.Split(url, "https://")[1]
+	} else if s.HasPrefix(url, "http://") {
+		url = s.Split(url, "http://")[1]
+	}
 	if s.HasSuffix(url, "/") {
 		url = s.Split(url, "/")[0]
-	}	
-	// write the whole body at once
-    err := ioutil.WriteFile(s.Join([]string{url, ".txt"}, ""), []byte(str), 0644)
-    if err != nil {
-        panic(err)
-    }
+	}
+
+	_, err := os.Open(s.Join([]string{url, ".txt"}, "")) // For read access.
+	if err != nil {
+		str := PageRank.GetPageRankJson(urls, destinations)
+
+		// write the whole body at once
+		err := ioutil.WriteFile(s.Join([]string{url, ".txt"}, ""), []byte(str), 0644)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		return
+	}
+
 }
 func getStatus(w http.ResponseWriter, r *http.Request) {
 	var reqFromUser REQ_HANDLER
@@ -84,19 +96,21 @@ func getStatus(w http.ResponseWriter, r *http.Request) {
 			requrl = s.Split(requrl, "http://")[1]
 		}
 		if s.HasSuffix(requrl, "/") {
-		requrl = s.Split(requrl, "/")[0]
+			requrl = s.Split(requrl, "/")[0]
 		}
 		file, err := ioutil.ReadFile(s.Join([]string{requrl, ".txt"}, ""))
 		if err != nil {
-        	w.Write([]byte(`{"message": "No file"}`))
-        	return
-       	} else {
-       		textNum := []byte(`{ "message":"Found", "content":[`) //file"] }"}
-       		textNum = append(textNum,file...)
-       		textNum = append(textNum,`]}`...)
-       		w.Write([]byte(textNum))
-       		return
-       	}
+			w.WriteHeader(200)
+			w.Write([]byte(`{"message": "No file"}`))
+			return
+		} else {
+			textNum := []byte(`{ "message":"Found", "content":[`) //file"] }"}
+			textNum = append(textNum, file...)
+			textNum = append(textNum, `]}`...)
+			w.WriteHeader(200)
+			w.Write([]byte(textNum))
+			return
+		}
 	} else {
 		w.WriteHeader(404)
 		fmt.Println(err, " User Req URL:", reqFromUser)
@@ -108,6 +122,6 @@ func ServeJson() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/getPageRank", serveRest).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/getStatus", getStatus).Methods("POST", "OPTIONS")
-	//http.ListenAndServe(":"+os.Getenv("PORT"), router)
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":"+os.Getenv("PORT"), router)
+	//http.ListenAndServe(":8080", router)
 }
